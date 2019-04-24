59937855a9d82e07ea1d0413a42e1b9e0d450647
vnherdeiro <victor.oriedreh@gmail.com>
2019-04-24 13:21:07 +0100

    minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,37 +48,26 @@ class tfGraph {
         return tf.mul(-1, this._rec_pred(node.args[0])); //assuming unary operator => unary minus
       }
       case 'ConstantNode':
-      // console.log(node.type, node.value);
       return tf.variable( tf.scalar(node.value), false);
       case 'SymbolNode':
-      // console.log( node);
       if (node.name.toLowerCase() === 'exp'){ //exp operator call
-        // console.log( node.args)
         return tf.exp( this._rec_pred(node.args[0]));
       }
       else if (node.name.toLowerCase() === 'log'){ //log operator
         return tf.log( this._rec_pred(node.args[0]));
       }
       else if (node.name.toLowerCase() === 'x'){ //x input variable -- maybe store it separately to build model(input, output)
-        // this.x_input = tf.input( {shape:[1,], name:'x'});
-        // this.x_input = tf.variable( tf.scalar(0), false, node.name ); //giving initial value in the future
-        // console.log( 'defined x', this.x_input)
         return this.x_input;
       }
       return this.varDic[ node.name];
-      // return tf.variable( tf.scalar(0), true, node.name ); //giving initial value in the future
       case 'FunctionNode':
-      // console.log('function node', node.type, node.name, node['fn']['name'], tf[node['fn']['name']] !== undefined)
       return tf[node.name]( this._rec_pred(node.args[0])); //assuming unary operation
       case 'ParenthesisNode':
       return this._rec_pred(node.content);
       default:
       console.log('Other node', node)
     }
-  };
-
-
-
+  }
 
   pred(){
     return this._rec_pred(this.formula_head)
@@ -89,17 +78,6 @@ class tfGraph {
     return pred.sub( this.y_input).square().mean();
   }
 
-  train(x,y){
-    let optimizer = tf.train.adam(1e-2);
-    this.x_input = tf.tensor1d( x, 'float32');
-    this.y_input = tf.tensor1d( y, 'float32');
-      // for( let t =0; t < 2e1; t++){
-      for( let t =0; t < 2e3; t++){
-      optimizer.minimize(() => {
-        return this.loss();
-      });
-    }
-  }
 
   printValues(){
     for( let name of Object.keys(this.varDic)){
@@ -109,22 +87,18 @@ class tfGraph {
   }
 
 
-    allocate_variables(){
-      // console.log('inside', this.varDic);
-      let tempDic = {};
+  allocate_variables(){
+    let tempDic = {};
     this.formula_head.traverse(function (node, path, parent) {
       if (node.type === 'SymbolNode' && ReservedParams.includes(node.name.toLowerCase())) {
         // console.log( tempDic, node)
         tempDic[node.name] = tf.variable( tf.scalar(1), true, node.name, 'float32');
       }
     });
-    // this.variables = variables.sort();
     this.varDic = tempDic;
-    // console.log( this.variables);
   }
 
-    trainStart(x, y, trainingState){
-  // trainStart(duration=3000, batch=10000, isTraining){
+  trainStart(x, y, trainingState){
     this.optimizer = tf.train.adam(1e-2);
     this.x_input = tf.tensor1d( x, 'float32');
     this.y_input = tf.tensor1d( y, 'float32');
@@ -133,25 +107,22 @@ class tfGraph {
     this.trainingState.next( 2);
     this._running = true;
     this.timer = setTimeout( () => this.trainStep());
-    // setTimeout( () => {this.trainStop(); isTraining.next(false)}, duration);
-    // let resolve = () => {this.trainStop();};
-    // await new Promise(resolve => setTimeout(resolve, duration));
   }
 
-    trainStep(){
-      for( let t =0; t < 50; t++){
-     this.optimizer.minimize(() => {
+  trainStep(){
+    for( let t =0; t < 50; t++){
+      this.optimizer.minimize(() => {
         return this.loss();
       });
     }
-      if( this._running){
-    this.timer = setTimeout( () => this.trainStep());
+    if( this._running){
+      this.timer = setTimeout( () => this.trainStep());
+    }
+    else{
+      this.trainingState.next(0);
+    }
   }
-  else{
-    this.trainingState.next(0);
-  }
-  }
-  
+
   trainStop(){
     this._running = false;
     clearTimeout( this.timer);
@@ -163,60 +134,56 @@ class tfGraph {
 
 
 const OPTION_TEMPLATE = {
-    // backgroundColor: '#191919', // matching optimum background color
-    title: {
-        text: ''
+  title: {
+    text: ''
+  },
+  legend: {
+    data: []
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
+  xAxis: {
+    name: 'x',
+    splitLine: {
+      show: false
     },
-    legend: {
-        data: []
+    axisTick: {
+      alignWithLabel: true,
     },
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'cross',
-            label: {
-                backgroundColor: '#6a7985'
-            }
-        }
+    axisLabel: {
+      show: true,
+      formatter: function (value, index) {
+        return (value).toFixed(1);
+      },
     },
-    xAxis: {
-        name: 'x',
-        // data: [],
-        splitLine: {
-            show: false
-        },
-        axisTick: {
-            alignWithLabel: true,
-        },
-        axisLabel: {
-            show: true,
-            formatter: function (value, index) {
-                return (value).toFixed(1);
-            },
-        },
-        // min: 0,
+  },
+  yAxis: {
+    name: 'y',
+    splitLine: {
+      show: false
     },
-    yAxis: {
-        name: 'y',
-        splitLine: {
-            show: false
-        },
-        axisLabel: {
-            formatter: function (value, index) {
-                return (value).toFixed(1);
-            },
-        },
-        // min: 0,
+    axisLabel: {
+      formatter: function (value, index) {
+        return (value).toFixed(1);
+      },
     },
-    series: []
+  },
+  series: []
 };
 
 const CHART_SERIE_TEMPLATE = {
-    data: [],
-    type: 'line',
-    smooth: true,
-    name: '',
-    showSymbol: false,
+  data: [],
+  type: 'line',
+  smooth: true,
+  name: '',
+  showSymbol: false,
 };
 
 
@@ -228,7 +195,6 @@ const CHART_SERIE_TEMPLATE = {
 export class AppComponent {
   title = 'angular-curvefitting';
   mathFormulaInput:string = '';
-  // variables:string[] = [];
   df = undefined;
   df_columns:string[] = [];
   x_col = '';
@@ -259,9 +225,7 @@ export class AppComponent {
       return 'Stop';
     })
     );
-    // console.log('setting them up ');
     this.endOfTrainingObs = this.trainingState.pipe( pairwise(), filter( values => values[0] !== 0 && values[1] === 0));
-    //update policy vis when training is finished
     this.endOfTrainingObs.subscribe( x => this.updateFitLine());
 
   }
@@ -281,7 +245,6 @@ export class AppComponent {
   }
 
   XYchange(){
-    // console.log( this.x_col, this.y_col);
     let df = this.df.toDict();
     if( this.x_col !== ''){
       this.train_x =  <number[]>df[ this.x_col];
@@ -300,42 +263,28 @@ export class AppComponent {
       this.chart.series = [serie,]
       this.chart.legend.data = ['data',]
       this.chart = _.cloneDeep(this.chart);
-      // console.log( this.chart)
-      // console.log( this.train_x, this.train_y);
     }
   }
 
   updateFitLine(){
-      // adding fit line
-      let x_points = tf.linspace( Math.min(...this.train_x), Math.max(...this.train_x), 1000).dataSync();
-      let y_points = this.graph.pred( x_points).dataSync();
-      // let temp_x = x_points;
-      // let temp_y = y_points.dataSync();
-      let serie = _.cloneDeep(CHART_SERIE_TEMPLATE);
-      for( let index = 0; index < this.train_x.length; index++){
-        serie.data.push( [x_points[index], y_points[index]]);
-      }  
+    // adding fit line
+    let x_points = tf.linspace( Math.min(...this.train_x), Math.max(...this.train_x), 1000).dataSync();
+    let y_points = this.graph.pred( x_points).dataSync();
+    // let temp_x = x_points;
+    // let temp_y = y_points.dataSync();
+    let serie = _.cloneDeep(CHART_SERIE_TEMPLATE);
+    for( let index = 0; index < this.train_x.length; index++){
+      serie.data.push( [x_points[index], y_points[index]]);
+    }  
 
-      serie['name'] = 'fit';
-      this.chart.series = [this.chart.series[0], serie,];
-      this.chart.legend.data = ['data', 'fit']
-      this.chart = _.cloneDeep(this.chart);
-      for( let name of Object.keys( this.graph.varDic)){
-        this.fittedValues[ name] = this.graph.varDic[name].dataSync();
-      }
-      this.calculateRsquare();
-  }
-
-  fit(){
-    // console.log('training...')
-    // this.graph.printValues();
-    this.isFitting = true;
-    this.graph.train( this.train_x, this.train_y);
-    // console.log('trained')
-    // this.graph.printValues();
-    this.updateFitLine();
-    this.isFitting = false;
-    this.hasFitted = true;
+    serie['name'] = 'fit';
+    this.chart.series = [this.chart.series[0], serie,];
+    this.chart.legend.data = ['data', 'fit']
+    this.chart = _.cloneDeep(this.chart);
+    for( let name of Object.keys( this.graph.varDic)){
+      this.fittedValues[ name] = this.graph.varDic[name].dataSync();
+    }
+    this.calculateRsquare();
   }
 
   changeVarValue(key, newValue){
@@ -355,17 +304,17 @@ export class AppComponent {
   }
 
 
-      trainMode(){
-      if ( this.trainingState.getValue() === 0){
-        this.policy_update_sub = interval(1500).subscribe( x => this.updateFitLine());
-        this.graph.trainStart(this.train_x, this.train_y, this.trainingState);
-      }
-      else{
-        this.trainingState.next(1);
-        this.policy_update_sub.unsubscribe();
-        this.graph.trainStop();
-      }
+  trainMode(){
+    if ( this.trainingState.getValue() === 0){
+      this.policy_update_sub = interval(1500).subscribe( x => this.updateFitLine());
+      this.graph.trainStart(this.train_x, this.train_y, this.trainingState);
     }
+    else{
+      this.trainingState.next(1);
+      this.policy_update_sub.unsubscribe();
+      this.graph.trainStop();
+    }
+  }
 
 
 }
